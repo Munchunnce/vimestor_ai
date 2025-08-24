2c815a9e2a9ab4098482ec20b84c8d89375286c7
Vimal <vimalkumarak478@gmail.com>
2025-08-24 19:00:21 +0530

    cors error  update vkc

--- backend/agent.js
+++ backend/agent.js
@@ -204,7 +204,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
